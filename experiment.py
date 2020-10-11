e3766e6a8b55bbf2be969e1115883f64c7b11758
bogus215 <wlstn215@gmail.com>
2020-10-11 16:51:21 +0900

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -81,29 +81,6 @@ for data in ["CIFAR","MNIST"]:
             print('failed')
 
 
-#%% for ready fine tune
-batch_size = [128,64,32]
-for data in ["CIFAR","MNIST"]:
-    for ind, data_size in enumerate(['50000','10000','1000']):
-
-        script_descriptor = open("train_autoencoder_fine.py", encoding ='utf-8')
-        a_script = script_descriptor.read()
-        if data == "MNIST":
-            sys.argv = ["train_autoencoder_fine.py", "--epoch", "1000",'--printevery','100','--experiment',f'{data}_ready_fine_tune_datasize_{data_size}','--dataset',f'{data}',
-                       '--input_dim','28','--input_dim_channel','1','--batch_size',f'{batch_size[ind]}']
-        else:
-
-            sys.argv = ["train_autoencoder_fine.py", "--epoch", "1000",'--printevery','100','--experiment',f'{data}_ready_fine_tune_datasize_{data_size}','--dataset',f'{data}',
-                       '--input_dim','32','--input_dim_channel','3' , '--batch_size' , f"{batch_size[ind]}"]
-
-        try:
-            print(sys.argv)
-            print('start')
-            exec(a_script)
-            gc.collect()
-        except:
-            print('failed')
-
 #%% classification using pretrained filter
 batch_size = [128,64,32]
 for data in ["CIFAR","MNIST"]:
@@ -121,7 +98,6 @@ for data in ["CIFAR","MNIST"]:
 
             sys.argv = ["train_classifier_fine.py", "--epoch", "1000",'--printevery','100','--experiment',f'{data}_fine_classification_datasize_{data_size}','--dataset',f'{data}',
                        '--input_dim','32','--input_dim_channel','3' , '--batch_size' , f"{batch_size[ind]}"]
-
 
         try:
             print(sys.argv)
